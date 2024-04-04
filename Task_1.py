3f9803838ee001507f4ff5c1e96356bf6105f57a
Aqil <166004777+Aqola@users.noreply.github.com>
2024-04-05 00:12:53 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,16 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a-input()
+  while True:
+    b=a.find("/")|
+    if b>=0:
+      c=a[:b]
+      print (c)
+      a=a[b+1:]
+    else:
+      print (a)
+      break
   pass
 
 if __name__ == "__main__":
