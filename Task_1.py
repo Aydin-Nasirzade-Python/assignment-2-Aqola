47818e539c7d4dc4d8655cd9ca5a265c95905098
Aqil <166004777+Aqola@users.noreply.github.com>
2024-04-05 09:15:26 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,14 +1,14 @@
 #import libraries here
 
 def main():
-  a="bcdfghjklmnpqrstuvwxz"
+  a="y"
   b="aeiou"
   k=input("Enter a letter of the alphabet: ")
-  if k in a:
-    print("Entered alphabet is a consonant!")
-  elif k in b:
+  if k in b:
     print("Entered alphabet is a vowel!")
-  else: print("Sometimes it is a vowel, and sometimes it is a consonant!") 
+  elif k in a:
+    print("Sometimes it is a vowel, and sometimes it is a consonant!")
+  else: print("Entered alphabet is a consonant!")
   pass
 
 if __name__ == "__main__":
